cdb50b3c4a6bb6b12e2bef6afaf801002d4f480f
ThinkCentre <jwangr@ymail.com>
2025-10-22 14:38:57 +1100

    Added google fonts

---
+++ skydance-app/src/app/styles/colors.js
@@ -0,0 +1,12 @@
+export const colors = {
+  background1: '#FFFFFF',
+  background2: '#4A5759',
+  background3: '#B0C4B1',
+  background4: '#DEDBD2',
+  background5: '#F7E1D7',
+  background6: '#EDAFB8cc',
+  paragraph2: '#FFFFFF',
+  paragraph1: '#000000',
+  accent1: '#BA8C93',
+  accent2: '#F2F0F1',
+};
---
+++ skydance-app/tailwind.config.js
@@ -0,0 +1,28 @@
+export const theme = {
+  extend: {
+    fontFamily: {
+      heading: ["Aboreto", "cursive"],
+      body: ["EB_Garamond", "serif"],
+    },
+    fontSize: {
+      h1: ["144px", "110px"], // fontSize, lineHeight
+      h2: ["50px", "120px"],
+      h3: ["32px", "120px"],
+      h4: ["32px", "110px"],
+      h5: ["28px", "110px"],
+      p: ["16px", "24px"],
+    },
+    colors: {
+      background1: "#FFFFFF",
+      background2: "#4A5759",
+      background3: "#B0C4B1",
+      background4: "#DEDBD2",
+      background5: "#F7E1D7",
+      background6: "#EDAFB8cc", // already 80% opacity
+      paragraph1: "#000000",
+      paragraph2: "#FFFFFF",
+      accent1: "#BA8C93",
+      accent2: "#F2F0F1",
+    },
+  },
+};
