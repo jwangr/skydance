0db64118ac7250f07dd5f500d23e0cd4386e0ae8
ThinkCentre <jwangr@ymail.com>
2025-11-08 20:37:27 +1100

    Blank studio hiring page

---
+++ skydance-app/src/app/hire/page.js
@@ -0,0 +1,19 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+export default function StudioPage() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "90vh",
+        margin: "auto",
+        textAlign: "center",
+      }}
+    >
+      <h5>Studio Hiring Page coming soon...</h5>
+    </Box>
+  );
+}
