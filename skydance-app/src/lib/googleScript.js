b47283126b771829c35f1d8115cef4c117be96a9
ThinkCentre <jwangr@ymail.com>
2025-11-08 20:30:35 +1100

    Fixed send email function and email formatting

--- skydance-app/src/lib/googleScript.js
+++ skydance-app/src/lib/googleScript.js
@@ -1,2 +1,2 @@
 export const GOOGLE_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbxO-kPvJcY2yUw7ioeSz1YGCEWiwn3wjZCRZTACJ5HznNfWAo7yW1fWOBWXhChcqGg/exec";
+"https://script.google.com/macros/s/AKfycbw14YXu3OSmg1OJ4l-gadS1Ufeo5s341-OtbOqY3frcC_10YmWN44oI4clyrF6YYW-dUQ/exec"
