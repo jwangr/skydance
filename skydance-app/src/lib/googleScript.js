b07a473baa7993201c193b9502c5ce0e52b7a7e1
ThinkCentre <jwangr@ymail.com>
2026-01-02 23:25:24 +1100

    Edited google scripts

--- skydance-app/src/lib/googleScript.js
+++ skydance-app/src/lib/googleScript.js
@@ -1,2 +1,2 @@
 export const GOOGLE_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbyGRfHEpk645KoI1KMiBkMVvw7b8OfCkCmL7X9gxAIWRwWSTGUPsaZeXIhmCp4_rHwOGA/exec";
+  "https://script.google.com/macros/s/AKfycbygxYmajF8DBKmFOuML4hK1UZiKPYw5BKWQacYl4K5COsZzi_YwR_DIi9Cg365Tonf1aw/exec";
