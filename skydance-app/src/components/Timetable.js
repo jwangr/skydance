e2d28d58312382dbfbdadd544d96f0b2cd1663b3
ThinkCentre <jwangr@ymail.com>
2026-01-24 22:22:07 +1100

    Removed console.log statement

--- skydance-app/src/components/Timetable.js
+++ skydance-app/src/components/Timetable.js
@@ -3,7 +3,6 @@ import { Box } from "@mui/material";
 import React from "react";
 
 export default function Timetable({ timetable = newestTimetable }) {
-  console.log(timetable);
   return (
     <Box
       sx={{
