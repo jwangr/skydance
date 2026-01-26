42218e7fab6a75e4dda80d0ef340d461d6256cd6
ThinkCentre <jwangr@ymail.com>
2026-01-26 21:01:15 +1100

    added tutoring descriptions

--- skydance-app/src/app/class/tutoring/page.js
+++ skydance-app/src/app/class/tutoring/page.js
@@ -11,53 +11,37 @@ import TutoringEnrolment from "@/components/forms/tutoringEnrolment";
 export default function TutoringPage() {
   return (
     <>
-      <Hero minHeight={"100vh"}>
-        <AnimateInView>
-          <h1>Tutoring Classes</h1>
-        </AnimateInView>
-      </Hero>
+      {/* Heading */}
+      <Box sx={{ margin: 5 }}>
+        <h1 style={{ marginX: "auto", textAlign: "center", color: "white" }}>
+          Tutoring Classes
+        </h1>
+      </Box>
 
-      {/* Music Description  */}
-      <SnapScrollSection visibility={1}>
-        <Box
-          sx={{
-            marginY: 10,
-            paddingX: { xs: 2, md: 10 },
-            textAlign: "center",
-            maxWidth: "lg",
-            marginX: "auto",
-          }}
-        >
-          <Stack gap={{ xs: 1, md: 3 }}>
-            <p>
-              <em>Tutoring for many subjects: Lorum Ipsum</em>
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-            <p>
-              Tutoring helps students understand ideas clearly and build
-              confidence. Lessons are tailored to each student’s pace and goals,
-              with a focus on asking questions and learning step by step.
-            </p>
-          </Stack>
-        </Box>
-      </SnapScrollSection>
-
-      {/* Pictures */}
-      <Grid container spacing={0} sx={{ alignItems: "stretch" }} margin={5}>
-        {artImages.map((item) => (
-          <Grid key={item.title} size={{ xs: 12, md: 4 }}>
-            <HeroGrows
-              backgroundImage={item.img}
-              overlay="rgba(0, 0, 0, 0)"
-              textShadow="0"
-              height="30vh"
-            ></HeroGrows>
-          </Grid>
-        ))}
-      </Grid>
+      {/* Tutoring Description  */}
+      <Box
+        sx={{
+          marginY: 10,
+          paddingX: { xs: 2, md: 10 },
+          textAlign: "center",
+          maxWidth: "lg",
+          marginX: "auto",
+        }}
+      >
+        <Stack gap={{ xs: 1, md: 3 }}>
+          <p>
+            At Sky Dance Studio, we promote learning for children from
+            Kindergarten to Year 7 with group tutoring classes. Our experienced
+            tutors help children with Maths, English or Science. We offer
+            classes on Tuesday, Wednesday and Saturday.
+          </p>
+          <p>
+            Tutoring helps students understand ideas clearly and build
+            confidence. Lessons are tailored to each student’s pace and goals,
+            with a focus on asking questions and learning step by step.
+          </p>
+        </Stack>
+      </Box>
 
       {/* Timetable */}
       <Timetable />
