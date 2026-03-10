6835a10b41ecaeec69b7b8208da2a8822cfeac27
ThinkCentre <jwangr@ymail.com>
2026-03-10 22:24:56 +1100

    Got rid of sender name

--- skydance-app/src/lib/googleScript.js
+++ skydance-app/src/lib/googleScript.js
@@ -1,2 +1,2 @@
 export const GOOGLE_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbxw3Sxyw1UFiWjgOsx12zAr2L-qbs0QhxB7_ZUmVUoxqi99wJNFNds3O3KaZgVEGNbiBQ/exec";
+  "https://script.google.com/macros/s/AKfycbyeb14vVOOzk9XHvfGHM4CIN9nZyOItS2H12InpLbpsZkg9H9NT2nRlZTEHESb9gMDP-w/exec";
