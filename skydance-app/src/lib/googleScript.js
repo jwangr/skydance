8fc8955e881574de5645fd9c4b704a1c7ee17ace
ThinkCentre <jwangr@ymail.com>
2026-03-10 21:52:56 +1100

    Changed sender address to sky dance studio email

--- skydance-app/src/lib/googleScript.js
+++ skydance-app/src/lib/googleScript.js
@@ -1,2 +1,2 @@
 export const GOOGLE_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbx4KhsGgG2FOD6HVTie5TcWHnSWZ6xbOCYaJ_3-4Va96P6-PwCiqLbX_itWGkgi3r95Nw/exec";
+  "https://script.google.com/macros/s/AKfycbwJ7hMRCHjLTg_0c6BmnzX0Nc00evpcng3_IxNb5Zz3B4ZrvfAJzLUByCADTPULMbXkqQ/exec";
