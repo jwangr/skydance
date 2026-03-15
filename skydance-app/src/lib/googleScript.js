5244736d4851900483a65124122bd65b71e4a962
ThinkCentre <jwangr@ymail.com>
2026-03-15 21:29:03 +1100

    Allow studio to reply directly back to customer

--- skydance-app/src/lib/googleScript.js
+++ skydance-app/src/lib/googleScript.js
@@ -1,2 +1,2 @@
 export const GOOGLE_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbzqUW_o_PC_RLLgGGp23BGR1EeAYeZ2OihLnBtb9YIe6uV3_sZXRAFTawB8UthGkFKaOg/exec";
+  "https://script.google.com/macros/s/AKfycbwSBcBed_bAJlSv-c3HEgmuiIScxilCTGOVn5AealOaOIdL36jNdeo2gIeuZ63TKWSDXw/exec";
